ec0d0c021124a5d1feb4cd0895b84a7a1aa57ef0
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-05-09 16:56:23 +0200

    feat: add  Milestone 3-4

--- conntrollers/postsController.js
+++ conntrollers/postsController.js
@@ -12,4 +12,32 @@ function index(req, res) {
     })
 };
 
-module.exports = { index }
+// Show
+
+function show(req, res) {
+    const id = req.params.id
+    // il ?  e un segnaposto 
+    const sql = `SELECT * FROM posts WHERE id = ?`;
+
+    // eseguiamo la query, gli passiamo un arry come secondo argomento che contiene i valori che antranno a sostituire il ?
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (results.length === 0) return res.status(404).json({ error: 'Post non trovato' })
+        res.json(results[0]);
+    })
+}
+
+// Delete
+
+function destroy(req, res) {
+    const id = req.params.id
+
+    const sql = 'DELETE FROM posts WHERE id = ?'
+    // prepariamo la query
+    connection.query(sql, [id], (err) => {
+        if (err) return res.status(500).json({ error: 'Failed to delete post' });
+        res.sendStatus(204)
+    })
+}
+
+module.exports = { index, show, destroy }
--- routers/posts.js
+++ routers/posts.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const postsController = require('../conntrollers/postsController')
-
+// idex
 router.get('/', postsController.index)
+// show
+router.get('/:id', postsController.show)
+//delete
+router.delete('/:id', postsController.destroy)
 
 module.exports = router
